fd64c2fc6a7411f7c35499e78a7b563b6a319c8c
andreslb9 <144810780+andreslb9@users.noreply.github.com>
2025-04-06 23:44:29 +0200

    index

---
+++ views/pages/index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index Page</title>
+</head>
+<body>
+    <h1>Welcome to the Index Page</h1>
+    <ul>
+        <li><a href="/viewData">View Data</a></li>
+        <li><a href="/addData">Add Data</a></li>
+        <li><a href="/deleteData">Delete Data</a></li>
+    </ul>
+</body>
+</html>
